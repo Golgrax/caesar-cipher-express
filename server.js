b274ef0c94dd6e2e9d96e445b5fb81249e9cb50e
Karl Benjamin Bughaw <pepe.kotelurow@gmail.com>
2025-05-09 10:36:26 +0000

    new file:   package-lock.json 	new file:   pakage.json 	new file:   public/index.html 	new file:   server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Main route to serve the Caesar Cipher tool
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Caesar Cipher app running at http://localhost:${port}`);
+});
